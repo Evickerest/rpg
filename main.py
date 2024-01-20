7389dd1b50c3ffc3051283617676560311602dc4
Eric Moras <121898077+Evickerest@users.noreply.github.com>
2024-01-20 04:03:38 +0000

    changes

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 print("Hello World!")
 
+# just writing something
+while True:
+    answer = input("Choose your Class: Melee Ranged Magic\n")
+    if answer.lower() not in ("melee", "ranged", "magic"):
+        print("Input not understood.\n")
+    else:
+        print(f"You selected {answer}!")
+        break
+    
+
+
 
 
 
