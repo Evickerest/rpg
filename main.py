a66c4d3840e9fb03889c1db3069034f51f962b2c
Eric Moras <121898077+Evickerest@users.noreply.github.com>
2024-01-31 13:58:40 +0000

    some changes

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-print("Hello World!")
+import tkinter as tk
 
-# just writing something
-while True:
-    answer = input("Choose your Class: Melee Ranged Magic\n")
-    if answer.lower() not in ("melee", "ranged", "magic"):
-        print("Input not understood.\n")
-    else:
-        print(f"You selected {answer}!")
-        break
-    
+window = tk.Tk()
 
-
-
-
-
+window.mainloop()
